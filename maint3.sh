5f26c8f68f33144f8762bf4208ff57b2eaa22967
Colin B. Macdonald <cbm@m.fsf.org>
2026-05-12 17:01:41 -0700

    plom-client has dependencies beyond plom-common

--- maint3.sh
+++ maint3.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-set -e
-
 # flatpak-pip-generator does not do wheels, but this newer tool does
+
+set -e
 
 # must match the python3 of the base image
 archy="311-x86_64 311-aarch64 312-x86_64 312-aarch64 313-x86_64 313-aarch64 314-x86_64 314-aarch64"
 
-req2flatpak --requirements-file requirements.txt \
-	--target-platforms ${archy} --outfile dependencies.yaml
 
+# plom-common and its dependencies
+
+req2flatpak --requirements-file requirements.txt \
+	--target-platforms ${archy} --outfile python3-plom-common.yaml
+
+
+# now build the yaml files for deps of plom-client (that are not deps of plom-common)
+
+req2flatpak --requirements \
+	Pillow==12.2.0 \
+	--target-platforms ${archy} --outfile python3-Pillow.yaml
+
+req2flatpak --requirements \
+        platformdirs==4.9.6\
+	--target-platforms ${archy} --outfile python3-platformdirs.yaml
+
+req2flatpak --requirements \
+        pyspellchecker==0.9.0 \
+	--target-platforms ${archy} --outfile python3-pyspellchecker.yaml
